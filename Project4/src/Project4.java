1c563bfb54888d32d86abf427c61210b8939719d
polymeter <polymeter@5254dd3d-353e-0410-baff-fb52aa316383>
2007-11-06 18:49:56 +0000

    Got the general parts finished and tested to make sure it evaluates things okay. Things to do: the evolutionary operators (crossover, mutation, reproduction), creating a random initial population of M candidate solutions (only creates one 30-bit string for now), displaying printout requirements in #6 (best of gen, worst, etc.), and  displaying printout requirements in #11 (best of run solution, # of generations, etc.).

---
+++ Project4/src/Project4.java
@@ -0,0 +1,16 @@
+
+/*
+ * This program will evolve a solution (x,y,z) that minimizes the function
+ * of the form |A * (x^3) + B * (y^2) + C * z|. 
+ * 
+ */
+
+
+public class Project4
+{
+	public static void main(String[] args)
+	{
+		GeneticAlgorithm genAl = new GeneticAlgorithm();
+		genAl.run();
+	}
+}
